c1c689c43118173885099bc138ed048c5f436795
voidarc <110408795+voidarclabs@users.noreply.github.com>
2024-01-23 09:18:37 +0000

    initial

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const express = require('express');
+const socketio = require('socket.io');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '/public'));
+});
+
+const server = app.listen(3000, () => {
+    console.log('Server running!')
+});
+
+const io = socketio(server)
+
+io.on('connection', (socket) => {
+    console.log(socket)
+})
 
---
+++ public/frontend.js
@@ -0,0 +1,3 @@
+function files() {
+    document.getElementById('apps').src = 'apps/files.html'
+}
